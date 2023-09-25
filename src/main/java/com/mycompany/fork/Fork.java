a735026daa724a82612d3d4c0fe9bd6a517099e5
szlankapeter <szlankapeter@gmail.com>
2023-09-25 15:04:56 +0200

    commit

---
+++ src/main/java/com/mycompany/fork/Fork.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fork;
+
+/**
+ *
+ * @author szlanka.peter
+ */
+public class Fork {
+
+    public static void main(String[] args) {
+        
+        while() {
+            
+            
+        }
+        
+        
+    }
+}
